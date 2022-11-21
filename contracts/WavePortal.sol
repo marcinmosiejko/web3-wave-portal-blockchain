84b33ef94cfb8aab7601d239e7149e2717b8d37a
Marcin <marcinmosiejko@gmail.com>
2022-11-21 11:45:01 +0100

    Add storing data in smart contract state variable

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves; // state variable
+
     constructor() {
         console.log("Gm frens");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
