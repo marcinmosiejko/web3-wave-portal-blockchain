c24a1e55203e49bd02a4cdf3241f0a5b61132396
Marcin <marcinmosiejko@gmail.com>
2022-11-25 20:47:14 +0100

    Update run script

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,49 +6,38 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves; // state variable
-    mapping(address => uint256) waveCountsMap; // creates a namespace in which all possible keys exist, and values are initialized to 0/false
-    address[] waversAddresses; // creates dynamically sized array that will store addresses of wavers (as with mapping it's not possible to iterate the keys or find out how many keys exist, because they all exist.) to keep track of number of wavers and find out which one of them has a highest count of waves
+   
+    // When an event is emitted, it stores the arguments passed in transaction logs
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
+    // Struct is a custom datatype, allows to customize what we want to hold inside it
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+
+    // Array of Wave structs that lets us hold all the waves anyone ever sends
+    Wave[] waves;
 
     constructor() {
         console.log("Gm frens");
     }
 
-    function wave() public {
-        waveCountsMap[msg.sender] += 1;
-        waversAddresses.push(msg.sender);
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved %d times!", msg.sender, waveCountsMap[msg.sender]);
+        console.log("%s waved w/ message %s", msg.sender, _message);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+  
     function getTotalWaves() public view returns (uint256) {
-        uint256 waversCount;
-        uint256 highestWavesCount;
-        uint256 count;
-        waversCount = waversAddresses.length;
-
-
-        // Check for the highest waves count
-        for (uint i = 0; i < waversCount; i++) {
-            count = waveCountsMap[waversAddresses[i]];
-
-            if (count > highestWavesCount) {
-                highestWavesCount = count;
-            }
-        }
-
-        console.log("We have %d total waves and %d total wavers!", totalWaves, waversCount);
-        console.log("Highest wavers with %d waves:", highestWavesCount);
-
-        // Log addresses with the highest waves count
-        for (uint i = 0; i < waversCount; i++) {
-            count = waveCountsMap[waversAddresses[i]];
-
-             if (count == highestWavesCount) {
-                console.log("%s", waversAddresses[i]);
-            }
-        }
-
         return totalWaves;
     }
 }
